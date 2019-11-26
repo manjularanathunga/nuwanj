0541d9c511905bb290988fbd622fea488d2595ba
“Manjula <manjula.ranathunga.sl@gmail.com>
2019-11-26 23:34:50 +0800

    Initial commit

---
+++ src/test/java/com/nj/websystem/WebSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nj.websystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
