1a76b6d2cd3e45ff2ca5e3cc7e452f65d7631314
“Manjula <manjula.ranathunga.sl@gmail.com>
2019-12-02 10:07:31 +0800

    name change

--- src/main/resources/static/wapp/controller/LoginPageController.js
+++ src/main/resources/static/wapp/controller/LoginPageController.js
@@ -1,3 +1,11 @@
 app.controller('LoginPageController', function($scope,$rootScope, $http, $location, $window) {
     $rootScope.pageTitle = "Login";
+
+    $scope.loginToSystem = function() {
+
+    }
+
+    var validateField = function(fieldToValidate, errmessage){
+
+    }
 });
