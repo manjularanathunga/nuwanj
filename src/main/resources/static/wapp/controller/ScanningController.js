33ac9589798c7bbaef30b79c495070882cc939b0
“Manjula <manjula.ranathunga.sl@gmail.com>
2020-02-01 18:15:49 +0800

    Modal changes on Medical Test

--- src/main/resources/static/wapp/controller/ScanningController.js
+++ src/main/resources/static/wapp/controller/ScanningController.js
@@ -44,18 +44,40 @@ app.controller('ScanningController', function($scope, $rootScope, $http, $locati
             .then(function(resp) {
                 if (resp.data.success) {
                     var obj = resp.data.response;
-                    $scope.patientTest = obj.patientmedicaltest;
-                    $scope.patient = obj.patient;
-                    $scope.scanHistortyList = obj.bypatientidlist;
-                    $scope.patientScan = obj.patientscan;
-                    if($scope.patient.dateOfBirth){
-                        $scope.currentAge = (new Date().getFullYear() - new Date($scope.patient.dateOfBirth).getFullYear());
+                  try{
+                        $scope.patientTest = obj.patientmedicaltest;
+                    }catch (e) {
+                        console.log('error retreving patientTest');
+                      $scope.patientTest = {};
                     }
+                    try{
+                        $scope.patient = obj.patient;
+                        if($scope.patient.dateOfBirth){
+                            $scope.currentAge = (new Date().getFullYear() - new Date($scope.patient.dateOfBirth).getFullYear());
+                        }
+                    }catch (e) {
+                        console.log('error retreving patient');
+                        $scope.patient = {};
+                    }
+                    try{
+                        $scope.scanHistortyList = obj.bypatientidlist;
+                    }catch (e) {
+                        console.log('error retreving scanHistortyList');
+                        $scope.patientScan = [];
+                    }
+                    try{
+                        $scope.patientScan = obj.patientscan;
+                    }catch (e) {
+                        console.log('error retreving patientScan');
+                        $scope.patientScan = {};
+                    }
+
                     $scope.patientScan.scanNumber = $scope.patientScan.scanNumber;
                     $scope.patientScan.billingNumber = billingNumber;
                     $scope.patientScan.patientId = $scope.patient.patientId;
                     $scope.uicompo.showItem  = true;
                     $scope.uicompo.showSave  = false;
+
                 } else {
                     Pop.timeMsg('error', 'SEARCH SCAN', ' SCAN NUMMBER NOT FOUND ', 2000);
                 }
